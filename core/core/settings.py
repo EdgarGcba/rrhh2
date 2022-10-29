8cbb834895cb909d20cd4dee5a28b29a8d3cc6ac
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2022-10-29 18:54:30 -0300

    changes

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "crispy_forms",
-    "crispy_bootstrap5",
+    # "crispy_forms",
+    # "crispy_bootstrap5",
     'consultora',
     
 ]
@@ -142,9 +142,9 @@ STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static')
 )
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+# CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
